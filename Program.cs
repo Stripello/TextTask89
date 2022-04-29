93becb7e9798617d14c51af01a470b61d56ef11e
Vladlen Shaparev <shve@outlook.com>
2022-04-29 17:40:50 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using TextTask89;
+
+Console.WriteLine("Please, enter text to process.");
+var text = Console.ReadLine();
+
+var searchedPosition = TextOperation.PositionOfFalseBracket(text);
+
+if (searchedPosition == -1)
+{
+    Console.WriteLine("There is no false bracket in text.");
+}
+else
+{
+    Console.WriteLine($"Position of false bracket in text = {searchedPosition}");
+}
+
---
+++ TextOperation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTask89
+{
+    internal static class TextOperation
+    {
+        internal static int PositionOfFalseBracket(string text)
+        {
+            var bracketCounter = 0; ;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '(')
+                {
+                    bracketCounter++;
+                }
+                if (text[i] == ')')
+                {
+                    bracketCounter--;
+                }
+                if (bracketCounter < 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        internal static string Delatation(string text, int neededLength)
+        {
+            var answer = new StringBuilder();
+            var 
+
+
+            return answer.ToString();
+        }
+    }
+
+}
